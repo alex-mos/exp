45d01b7a9f3fdd4369fc4e51c11744a7e3d9be75
Alexander Mospan <alexander.mospan@gmail.com>
2015-12-11 19:23:56 +0300

    recursion exp

--- js/freestyle/walk_the_dom.js
+++ js/freestyle/walk_the_dom.js
@@ -22,5 +22,12 @@ var walk_the_DOM = function walk(node, func) {
 var getElementsByAttribute = function(att, value) {
 	var results = [];
 
-	walk_the_DOM(document.body, )
-}
+	walk_the_DOM(document.body, function(node) {
+		var actual = node.nodeType === 1 && node.getAttribute(att);
+		if (typeof actual === 'string' && (actual === value || typeof value === 'string')) {
+			results.push(node);
+		}
+	});
+
+	return results;
+};
