c7ac81d2d110cac4c91ab612017ea56ea4c4aef1
alexander.mospan <alexander.mospan@gmail.com>
2016-02-02 21:55:53 +0300

    coursera angular week 2. form validation

--- angular/coursera/conFusion/app/scripts/app.js
+++ angular/coursera/conFusion/app/scripts/app.js
@@ -84,6 +84,8 @@ app.controller('MenuController', ['$scope', function($scope) {
 
 }]);
 
+/* Contact Us page */
+
 app.controller('ContactController', ['$scope', function ($scope) {
 
 	$scope.feedback = {
@@ -111,7 +113,7 @@ app.controller('ContactController', ['$scope', function ($scope) {
 
 app.controller('FeedbackController', ['$scope', function ($scope) {
 
-	$scope.sendFeedback = function () {
+	$scope.sendFeedback = function() {
 
 		if ($scope.feedback.agree && $scope.feedback.mychannel === '') {
 
@@ -141,8 +143,63 @@ app.controller('FeedbackController', ['$scope', function ($scope) {
 
 }]);
 
+/* Dish Detail page */
+
+app.controller('dishDetailController', function () {
+
+	this.order = '';
+
+	this.dish = {
+		name: 'Uthapizza',
+		image: 'images/uthapizza.png',
+		category: 'mains',
+		label: 'Hot',
+		price: '4.99',
+		description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+		comments: [
+			{
+				rating: 5,
+				comment: "Imagine all the eatables, living in conFusion!",
+				author: "John Lemon",
+				date: "2012-10-16T17:57:28.556094Z"
+			},
+			{
+				rating: 4,
+				comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+				author: "Paul McVites",
+				date: "2014-09-05T17:57:28.556094Z"
+			},
+			{
+				rating: 3,
+				comment: "Eat it, just eat it!",
+				author: "Michael Jaikishan",
+				date: "2015-02-13T17:57:28.556094Z"
+			},
+			{
+				rating: 4,
+				comment: "Ultimate, Reaching for the stars!",
+				author: "Ringo Starry",
+				date: "2013-12-02T17:57:28.556094Z"
+			},
+			{
+				rating: 2,
+				comment: "It's your birthday, we're gonna party!",
+				author: "25 Cent",
+				date: "2011-12-02T17:57:28.556094Z"
+			}
+
+		]
+	};
+
+});
+
+
 app.controller('CommentController', ['$scope', function($scope) {
 
+	$scope.sendComment = function() {
 
+		$scope.commentForm.$setPristine();
+
+	}
 
 }]);
