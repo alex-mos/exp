a518dea700febbda403c30992ff7fe7340579fb2
Alexander Mospan <alexander.mospan@gmail.com>
2016-02-03 13:01:20 +0300

    node: simple webserver

--- node/server.js
+++ node/server.js
@@ -1,7 +1,20 @@
-var http = require('http');
+"use strict";
 
-var server = http.createServer(function(request, response) {
-    response.end('hello docker!!!');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 1337;
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+
+    res.end('Hello World\n');
+
+}).listen(port, hostname, () => {
+
+    console.log(`Server running at http://${hostname}:${port}/`);
+
 });
-
-server.listen(8000);
