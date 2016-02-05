649bad75be60472d1a788780186a2b4ff7a7df03
alexander.mospan <alexander.mospan@gmail.com>
2016-02-05 19:23:41 +0300

    add: node auth layout

--- node/eduonix/4-auth/routes/users.js
+++ node/eduonix/4-auth/routes/users.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+	res.send('respond with a resource');
+});
+
+router.get('/register', function (req, res, next) {
+	res.render('register', {
+		title: 'Register'
+	})
+});
+
+router.get('/login', function (req, res, next) {
+	res.render('login', {
+		title: 'Log In'
+	})
+});
+
+router.get('/logout', function (req, res, next) {
+	res.send('logout')
 });
 
 module.exports = router;
