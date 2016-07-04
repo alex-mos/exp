b7bd7a273356b9fa594af7a5e37958e33cbbee78
Alexander Mospan <alexander.mospan@gmail.com>
2016-07-04 16:19:49 +0300

    add: various js

--- node/class-db/index.js
+++ node/class-db/index.js
@@ -1,66 +1,61 @@
 const telnet = require('net');
 const WebSocketServer = require('ws');
+const uuid = require('node-uuid');
 
 
 // WebSocket-сервер на порту 8081
 var webSocketServer = new WebSocketServer.Server({
-  port: 8081
+	port: 8081
 });
 
 
-
 // Телнет-сервер на порту 1337
-var newSocket = function(socket) {
-  socket.write('Welcome to the Telnet server!\n Write me anything.');
+var newTelnetSocket = function (telnetSocket) {
+	telnetSocket.write('Welcome to the Telnet server!\n Write me anything.'); // Приветствуем клиента, подключившегося по телнету.
+
+	telnetSocket.on('error', function (err) {
+		console.log(err);
+	});
 
 
+	// обрабатываем веб-клиентов
+	var webSocketClients = {};
+
+	webSocketServer.on('connection', function (ws) {
+		console.log(ws);
+		var id = uuid.v4();
+		webSocketClients[id] = ws;
+		console.log("новое соединение " + id);
+
+		// ws.on('message', function(message) {
+		//   console.log('получено сообщение ' + message);
+		//
+		//   for (var key in webSocketClients) {
+		//     webSocketClients[key].send(message);
+		//   }
+		// });
+
+		telnetSocket.on('data', function (data) {
+			console.log(data.toString());
+
+			for (var key in webSocketClients) {
+				webSocketClients[key].send(data.toString());
+			}
+		});
+
+		ws.on('close', function () {
+			console.log('соединение закрыто ' + id);
+			delete webSocketClients[id];
+		});
+	});
 
 
-  var clients = {};
-
-  webSocketServer.on('connection', function(ws) {
-    var id = Math.random();
-    clients[id] = ws;
-    console.log("новое соединение " + id);
-
-    ws.on('message', function(message) {
-      console.log('получено сообщение ' + message);
-
-      for (var key in clients) {
-        clients[key].send(message);
-      }
-    });
-
-    socket.on('data', function(data) {
-      console.log(data.toString());
-
-      for (var key in clients) {
-        clients[key].send(data.toString());
-      }
-    });
-
-    ws.on('close', function() {
-      console.log('соединение закрыто ' + id);
-      delete clients[id];
-    });
-  });
-
-
-
-
-
-
-  socket.on('error', function(err) {
-    // throw err;
-    console.log(err);
-  });
 };
 
 // Create a new server and provide a callback for when a connection occurs
-var server = telnet.createServer(newSocket);
+var server = telnet.createServer(newTelnetSocket);
 
 server.listen(1337);
-
 
 
 
