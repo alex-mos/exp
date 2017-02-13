1da51bd675092df62001bca85c399604ad98ce3f
Alex Mos <alexander.mospan@gmail.com>
2017-02-14 00:32:39 +0300

    mark fizzbuzz

---
+++ js/freestyle/fizzbuzz.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+
+const fizzbuzz = (num) => {
+  if (!(num % 3) && !(num % 5)) {
+    return 'FizzBuzz'
+  } else if (!(num % 3)) {
+    return 'Fizz'
+  } else if (!(num % 5)) {
+    return 'Buzz'
+  } else {
+    return num
+  }
+}
+
+assert.equal(fizzbuzz(3), 'Fizz')
+assert.equal(fizzbuzz(5), 'Buzz')
+assert.equal(fizzbuzz(15), 'FizzBuzz')
+assert.equal(fizzbuzz(31), 31)
