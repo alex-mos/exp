6a1003f93d7ea44bafccdf87a0d2ca4d07a5e782
Alex Mos <alexander.mospan@gmail.com>
2017-02-15 23:18:58 +0300

    webpack kantor 3 init

---
+++ js/webpack/kantor/3_styles/frontend/main.js
@@ -0,0 +1,21 @@
+import Menu from './menu'
+
+let pandaMenu = new Menu({
+  title: 'Menu панды',
+  items: [
+    {
+      text: 'Яйца',
+      href: '#eggs'
+    },
+    {
+      text: 'Мясо',
+      href: '#meat'
+    },
+    {
+      text: '99% еды - бамбук',
+      href: '#bamboo'
+    }
+  ]
+})
+
+document.body.appendChild(pandaMenu.elem)
