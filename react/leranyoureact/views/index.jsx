aed68b955c67166cc3acc72ca8b805c07b9cb9c9
Alex Mos <alexander.mospan@gmail.com>
2017-03-02 15:41:56 +0300

    learnyoureact 4-7

--- react/leranyoureact/views/index.jsx
+++ react/leranyoureact/views/index.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 class TodoBox extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="todoBox">
         <h1>Todos</h1>
-        <TodoList />
+        <TodoList data={ this.props.data }/>
         <TodoForm />
       </div>
     )
@@ -14,13 +18,18 @@ class TodoBox extends Component {
 
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
+    const todos = this.props.data.map(todo => <Todo title={todo.title} key={todo.title}>{todo.detail}</Todo>)
+
     return (
       <div className='todoList'>
         <table style={{ border: '2px solid black' }}>
           <tbody>
-            <Todo title='Shopping'>Milk</Todo>
-            <Todo title='Hair cut'>13:00</Todo>
+            { todos }
           </tbody>
         </table>
       </div>
@@ -32,16 +41,33 @@ class TodoList extends Component {
 class Todo extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      checked: false
+    }
+  }
+
+  handleChange(e) {
+    console.log(this.props.title + ': ' + this.state.checked)
+    this.setState({ checked: e.target.checked })
   }
 
   render() {
     return (
       <tr>
-        <td style={{ border: '1px solid black'}}>{ this.props.title }</td>
-        <td style={{ border: '1px solid black'}}>{ this.props.children }</td>
+        <td style={ style.tableContent }>
+          <input type='checkbox'
+                 checked={ this.state.checked }
+                 onChange={ this.handleChange } />
+        </td>
+        <td style={ style.tableContent }>{ this.props.title }</td>
+        <td style={ style.tableContent }>{ this.props.children }</td>
       </tr>
     )
   }
+}
+Todo.propTypes = {
+  title: React.PropTypes.string.isRequired
 }
 
 
@@ -56,3 +82,9 @@ class TodoForm extends Component {
 }
 
 export default TodoBox
+
+let style = {
+  tableContent: {
+    border: '1px solid black'
+  }
+}
