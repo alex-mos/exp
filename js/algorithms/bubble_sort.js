1c5d43dcab2519261f532404ac51390123878e85
Alex Mos <alexander.mospan@gmail.com>
2017-03-21 13:49:44 +0300

    bubble sort completed

---
+++ js/algorithms/bubble_sort.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+
+const sort = (arr) => {
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+      let temp
+      if (arr[i] > arr[i + 1]) {
+        temp = arr[i + 1]
+        arr[i + 1] = arr[i]
+        arr[i] = temp
+      }
+    }
+  }
+  return arr
+}
+
+assert.deepEqual(sort([3, 1, 2]), [1, 2, 3])
+assert.deepEqual(sort([3, 4, 1, 2]), [1, 2, 3, 4])
