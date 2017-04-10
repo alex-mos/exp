66d56d88fca6447bdb9110125179734a03eb0163
Alex Mos <alexander.mospan@gmail.com>
2017-04-10 22:14:49 +0300

    codewars

---
+++ js/codewars/7-filter_arr_duplicates.js
@@ -0,0 +1,12 @@
+const assert = require('assert')
+
+// my
+const unique = (arr) => arr.filter((item, index) => !arr.slice(0, index).includes(item))
+
+// best
+const unique = (arr) => arr.filter((x, i) => arr.indexOf(x) === i)
+
+// test
+assert.deepEqual(unique([4]), [4])
+assert.deepEqual(unique([1, 2, 2, 3]), [1, 2, 3])
+assert.deepEqual(unique([8, 4, 5, 2, 7, 8, 1, 5, 4, 4]), [8, 4, 5, 2, 7, 1])
