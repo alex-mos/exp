bfdfd64e0f67ca1cb896634c3094c71b9e9a2fcf
Alex Mos <alexander.mospan@gmail.com>
2017-05-02 10:43:03 +0300

    insertion sort implementation

---
+++ js/algorithms/insertion_sort.js
@@ -0,0 +1,18 @@
+const assert = require('assert')
+
+const sort = (arr) => {
+  let i
+
+  for (let j = 1; j < arr.length; j++) {
+    i = j - 1
+    while (i > 0 && arr[i] > arr[j]) {
+      arr[i + 1] = arr[i]
+    }
+    arr[i + 1] = arr[j]
+  }
+
+  return arr
+}
+
+assert.deepEqual(sort([3, 1, 2]), [1, 2, 3])
+assert.deepEqual(sort([3, 4, 1, 2]), [1, 2, 3, 4])
