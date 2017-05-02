dcafe93573b8f73f0cca8ed0beee84ca1fe4acc7
Alex Mos <alexander.mospan@gmail.com>
2017-05-02 10:42:38 +0300

    binary search in progress

---
+++ js/algorithms/khan_binary_search.js
@@ -0,0 +1,73 @@
+/* Returns either the index of the location in the array,
+  or -1 if the array did not contain the targetValue */
+var doSearch = function(array, targetValue) {
+	var min = 0
+	var max = array.length - 1
+  var guess
+
+	function makeGuess () {
+     guess = Math.floor(((max - min) / 2) + min)
+  }
+  makeGuess()
+
+  while (arr[guess] !== targetValue) {
+    iterationCounter++
+    if (min > max) {
+      return -1
+    }
+    if (arr[guess] > targetValue) {
+      max = guess - 1
+      makeGuess()
+    } else {
+      min = guess + 1
+      makeGuess()
+    }
+  }
+
+  return guess
+}
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+var result = doSearch(primes, 73)
+// println("Found prime at index " + result)
+
+//Program.assertEqual(doSearch(primes, 73), 20)
+
+
+
+
+
+// binary search
+// const binSearch = (arr, targetValue) => {
+//   let iterationCounter = 0
+//   let min = 0
+//   let max = arr.length - 1
+//   let guess
+//   function makeGuess () {
+//      guess = Math.floor(((max - min) / 2) + min)
+//   }
+//   makeGuess()
+//
+//   while (arr[guess] !== targetValue) {
+//     iterationCounter++
+//     if (min > max) {
+//       return -1
+//     }
+//     if (arr[guess] > targetValue) {
+//       max = guess - 1
+//       makeGuess()
+//     } else {
+//       min = guess + 1
+//       makeGuess()
+//     }
+//   }
+//
+//   console.log(`binary search iterations: ${iterationCounter}`)
+//   return guess
+// }
+
+const assert = require('assert')
+
+assert.equal(doSearch(primes, 73), 20)
