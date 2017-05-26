60ed2a18fe6118f9cde5cdb0790e491a65f986c6
Alex Mos <alexander.mospan@gmail.com>
2017-05-26 23:25:51 +0300

    codewars colors

--- js/codewars/7-convert_color_to_grayscale.js
+++ js/codewars/7-convert_color_to_grayscale.js
@@ -1,4 +1,17 @@
 const assert = require('assert')
 
+const color2grey = function (image) {
+  return image.map(row => row.map(pixel => {
+    let sum = pixel.reduce((prev, current) => prev + current, 0)
+    let avg = Math.round(sum / 3)
+    return [avg, avg, avg]
+  }))
+}
 
-assert.equal()
+assert.deepEqual(color2grey([
+ [ [123, 231, 12], [56, 43, 124] ],
+ [ [78, 152, 76], [64, 132, 200] ]
+]),[
+ [ [122, 122, 122], [74, 74, 74] ],
+ [ [102, 102, 102], [132, 132, 132] ]
+])
