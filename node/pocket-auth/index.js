43c0525f3bfe2aa8bfaa26cb9f4b9d493d5928fb
Alex Mos <alexander.mospan@gmail.com>
2017-08-07 18:06:27 +0300

    rust and node-pocket-auth init

--- node/pocket-auth/index.js
+++ node/pocket-auth/index.js
@@ -1,3 +1,20 @@
+const qs = require('querystring')
+const request = require('request')
 const opn = require('opn')
 
-opn('http://inbox.google.com')
+
+let options = {
+  headers: {
+
+  }
+
+}
+
+request('getpocket.com/v3/oauth/request', (req(), res) => {
+
+  req.write(qs.stringify({
+    consumer_key: '51026-6da61e0b0e81e827f4bfff10',
+    redirect_uri: 'ya.ru'
+  }))
+
+})
