3c7fb6f84029ad270941ebd95827f8d74dbc8e7a
Alex Mos <alexander.mospan@gmail.com>
2017-08-08 16:23:14 +0300

    pocket auth problems and little rust

--- node/pocket-auth/index.js
+++ node/pocket-auth/index.js
@@ -1,20 +1,24 @@
 const qs = require('querystring')
 const request = require('request')
-const opn = require('opn')
+// const opn = require('opn')
 
 
 let options = {
-  headers: {
-
-  }
-
+    url: 'https://getpocket.com/v3/oauth/request',
+    headers: {
+        'method': 'POST',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Accept': 'application/json'
+    },
+    body: qs.stringify({
+        consumer_key: '51026-6da61e0b0e81e827f4bfff10',
+        redirect_uri: 'https://ya.ru'
+    })
 }
 
-request('getpocket.com/v3/oauth/request', (req(), res) => {
-
-  req.write(qs.stringify({
-    consumer_key: '51026-6da61e0b0e81e827f4bfff10',
-    redirect_uri: 'ya.ru'
-  }))
-
+request(options, (err, res, body) => {
+    console.log('error:', err);
+    console.log('statusCode:', res && res.statusCode)
+    console.log(res);
+    console.log('body:', body);
 })
