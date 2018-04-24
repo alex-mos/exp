c942acaf5b8fbf639ee49d5adafa22f56fe90941
Alexander Mospan <alexander.mospan@gmail.com>
2018-04-24 15:56:47 +0300

    vue-skills tutorial project in progress

---
+++ vue/vue-skills/src/components/Skills.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <div class="holder">
+        <ul>
+            <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+        </ul>
+    </div>
+
+    <div v-bind:class="{ alert: showAlert }">Alert</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data() {
+    return {
+      skills: [
+        { 'skill': 'Vue.js' },
+        { 'skill': 'Frontend developer' }
+      ],
+      showAlert: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .alert {
+        background-color: yellow;
+        width: 100%;
+        height: 30px;
+    }
+</style>
