a79ae5f7e6cc7a9ae756f43db3030a1da1bd0382
Alex Mos <alexander.mospan@gmail.com>
2018-04-24 20:58:01 +0300

    vue-skills input validation in progress

--- vue/vue-skills/src/components/Skills.vue
+++ vue/vue-skills/src/components/Skills.vue
@@ -1,34 +1,115 @@
 <template>
   <div class="hello">
     <div class="holder">
-        <ul>
-            <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
-        </ul>
+      <form @submit.prevent="addSkill">
+        <input
+          type="text"
+          placeholder="Enter a skill you have..."
+          v-model="skill"
+          v-validate="'min:5'"
+          name="skill">
+        <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </form>
+
+      <ul>
+        <li v-for="(data, index) in skills" :key="index">{{ index }}. {{ data.skill }}</li>
+      </ul>
     </div>
 
-    <div v-bind:class="{ alert: showAlert }">Alert</div>
+    <p>These are all skills that you possess.</p>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Skills',
-  data() {
-    return {
-      skills: [
-        { 'skill': 'Vue.js' },
-        { 'skill': 'Frontend developer' }
-      ],
-      showAlert: true
+  export default {
+    name: 'Skills',
+    data() {
+      return {
+        skill: '',
+        skills: [
+          { 'skill': 'Vue.js' },
+          { 'skill': 'Frontend developer' }
+        ]
+      }
+    },
+    methods: {
+      addSkill() {
+        this.skills.push({ 'skill': this.skill })
+        this.skill = ''
+      }
     }
   }
-}
 </script>
 
 <style scoped>
-    .alert {
-        background-color: yellow;
-        width: 100%;
-        height: 30px;
+  .holder {
+    background: #fff;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+
+  ul li {
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align: center;
+    padding: 30px 0;
+    color: gray;
+  }
+
+  .container {
+    box-shadow: 0 0 40px lightgray;
+  }
+
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
+  }
+
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
     }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  i {
+    float: right;
+    cursor: pointer;
+  }
 </style>
