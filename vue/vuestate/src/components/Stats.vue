b215df07b35628edcae4f2fd47198b0e435c25f9
Alex Mos <alexander.mospan@gmail.com>
2018-05-08 20:02:23 +0300

    vuex tutorial

---
+++ vue/vuestate/src/components/Stats.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="stats">
+    <h1>A different component</h1>
+    <p>There are currently {{ count }} links</p>
+  </div>
+</template>
+
+<script>
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: 'Stats',
+
+    computed: {
+      ...mapGetters({
+        count: 'countLinks'
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/vuestate/src/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    title: 'My Custom Title',
+    links: [
+      'https://google.com',
+      'https://khanacademy.com',
+      'https://youtube.com'
+    ]
+  },
+  getters: {
+    countLinks: state => {
+      return state.links.length
+    }
+  }
+  ,
+  mutations: {
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    }
+  },
+  actions: {
+
+  }
+})
