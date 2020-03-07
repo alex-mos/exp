a5797546dfe59b3ff43a0e508c245322dc6f62f8
Alex Mos <alexander.mospan@gmail.com>
2020-03-07 11:48:25 +0300

    added some exercises

---
+++ js/leetcode/array_remove_element.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  const result = nums.filter(function(num) {
+    return num !== val
+  })
+  // mutate initial array
+  nums.splice(0)
+  for (let num of result) {
+    nums.push(num)
+  }
+  return nums.length
+};
+
+let a = [0,1,2,2,3,0,4,2]
+assert.deepEqual(a, [0,1,2,2,3,0,4,2])
+assert.equal(removeDuplicates(a, 2), 5)
+assert.deepEqual(a, [0,1,3,0,4])
