f2428a87eb332007ea996be4e53095593b5f1da5
Alex Mos <alexander.mospan@gmail.com>
2020-03-16 23:15:33 +0300

    added react boilerplate app

---
+++ js/freecodecamp/react-lessons/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import './App.css';
+
+function App() {
+  return <App />
+}
+
+export default App;
---
+++ js/freecodecamp/react-lessons/src/components/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Kitten extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <h1>Hi</h1>
+    )
+  }
+}
