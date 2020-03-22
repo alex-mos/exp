4afb16e5954edc59b05e8e3fc47419079badede8
Alex Mos <alexander.mospan@gmail.com>
2020-03-22 19:26:44 +0300

    added redux boilerplate app

---
+++ js/freecodecamp/redux/lesson.js
@@ -0,0 +1,31 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      return Object.assign(state, { status: 'online'})
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+
+
+const store = createStore(immutableReducer)
+
+console.log(store.getState())
+store.dispatch(wakeUp())
+console.log(store.getState())
