91017c09e5d8d9de4da0e40e1f44fee0c8c692e2
Alexander Mospan <alexander.mospan@gmail.com>
2022-03-24 12:01:38 +0300

    interview prep

---
+++ js/interview-preparation/factorial.js
@@ -0,0 +1,32 @@
+// факториал от пяти это
+// 1 * 2 * 3 * 4 * 5
+//     2   6   24  120
+// factoral(5) === 120
+// factoral(1) === 1
+// factoral(0) === 1
+
+function factorialCycle(number) {
+  let result = 1
+  for (let i = 1; i < number; i++) {
+    result *= i + 1
+  }
+  return result
+}
+
+console.log(factorialCycle(0))
+console.log(factorialCycle(1))
+console.log(factorialCycle(5))
+
+
+// рекурсия — это вызов функции
+function factorialRecursion(number) {
+  if (number <= 1) {
+    return 1;
+  } else {
+    return number * factorialRecursion(number - 1);
+  }
+}
+
+console.log(factorialRecursion(0))
+console.log(factorialRecursion(1))
+console.log(factorialRecursion(5))
---
+++ js/interview-preparation/fibonacci.js
@@ -0,0 +1,7 @@
+function fib(n) {
+
+}
+
+console.log(fib(3)); // 2
+console.log(fib(7)); // 13
+console.log(fib(77)); // 5527939700884757
