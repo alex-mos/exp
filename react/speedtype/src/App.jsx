e29bc5033fe4df3901d70583dc14e0b43fea927d
Aleksandr Mospan <alexander.mospan@gmail.com>
2023-04-01 18:14:13 +0200

    react speedtype test app

---
+++ react/speedtype/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Typing Speed Test</h1>
+      <form>
+        <textarea name="text"> </textarea>
+        <h4>Time remaining: ???</h4>
+        <button type="button">Start</button>
+      </form>
+      <h1>Words: ???</h1>
+    </div>
+  );
+}
+
+export default App;
