5246b5c4a43468def044472ed1ed8ad006e7a7bd
Aleksandr Mospan <alexander.mospan@gmail.com>
2024-08-24 13:44:30 +0300

    courses update

--- c/pikuma-gameloop/src/main.c
+++ c/pikuma-gameloop/src/main.c
@@ -1,171 +1,123 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 #include "./constants.h"
 
-int game_is_running = FALSE;
+bool game_is_running = false;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-int last_frame_time = 0;
+struct ball {
+  float x;
+  float y;
+  int width;
+  int heigth;
+} ball;
 
-struct game_object {
-    float x;
-    float y;
-    float width;
-    float height;
-    float vel_x;
-    float vel_y;
-} ball, paddle;
+bool initialize_window(void) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    fprintf(stderr, "Error initializing SDL.\n");
+    return false;
+  }
 
-int initialize_window(void) {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        fprintf(stderr, "Error initializing SDL.\n");
-        return FALSE;
-    }
-    window = SDL_CreateWindow(
-        NULL,
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
-        SDL_WINDOW_BORDERLESS
-    );
-    if (!window) {
-        fprintf(stderr, "Error creating SDL Window.\n");
-        return FALSE;
-    }
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    if (!renderer) {
-        fprintf(stderr, "Error creating SDL Renderer.\n");
-        return FALSE;
-    }
-    return TRUE;
+  window = SDL_CreateWindow(
+    "game", // window title
+    SDL_WINDOWPOS_CENTERED, // window x position
+    SDL_WINDOWPOS_CENTERED, // window y position
+    WINDOW_WIDTH, // window width
+    WINDOW_HEIGHT, // window height
+    0
+    // SDL_WINDOW_FULLSCREEN
+    // SDL_WINDOW_BORDERLESS
+  );
+
+  if (!window) {
+    fprintf(stderr, "Error creating SDL Window.\n");
+    return false;
+  }
+
+  renderer = SDL_CreateRenderer(
+    window,
+    -1, // default display driver
+    0 // default way of rendering
+  );
+
+  if (!renderer) {
+    fprintf(stderr, "Error creating SDL Renderer.\n");
+    return false;
+  }
+
+  return true;
 }
 
-void process_input(void) {
-    SDL_Event event;
-    SDL_PollEvent(&event);
+void process_input() {
+  SDL_Event event;
+  SDL_PollEvent(&event);
 
-    switch (event.type) {
-        case SDL_QUIT:
-            game_is_running = FALSE;
-            break;
-        case SDL_KEYDOWN:
-            if (event.key.keysym.sym == SDLK_ESCAPE)
-                game_is_running = FALSE;
-            if (event.key.keysym.sym == SDLK_LEFT)
-                paddle.vel_x = -400;
-            if (event.key.keysym.sym == SDLK_RIGHT)
-                paddle.vel_x = +400;
-            break;
-        case SDL_KEYUP:
-            if (event.key.keysym.sym == SDLK_LEFT)
-                paddle.vel_x = 0;
-            if (event.key.keysym.sym == SDLK_RIGHT)
-                paddle.vel_x = 0;
-            break;
-    }
+  switch (event.type) {
+    case SDL_QUIT: // quit button on the window
+      game_is_running = false;
+      break;
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_ESCAPE) {
+        game_is_running = false;
+      } else if (event.key.keysym.sym == SDLK_UP) {
+        ball.y = ball.y - 2;
+      } else if (event.key.keysym.sym == SDLK_DOWN) {
+        ball.y = ball.y + 2;
+      }
+      break;
+  }
 }
 
-void setup(void) {
-    ball.width = 15;
-    ball.height = 15;
-    ball.x = 20;
-    ball.y = 20;
-    ball.vel_x = 300;
-    ball.vel_y = 300;
-
-    paddle.width = 100;
-    paddle.height = 20;
-    paddle.x = (WINDOW_WIDTH / 2) - (paddle.width / 2);
-    paddle.y = WINDOW_HEIGHT - 40;
-    paddle.vel_x = 0;
-    paddle.vel_y = 0;
+void setup() {
+  ball.x = 20;
+  ball.y = 20;
+  ball.width = 15;
+  ball.heigth = 15;
 }
 
-void update(void) {
-    // Sleep the execution until we reach the target frame time in milliseconds
-    int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - last_frame_time);
-
-    // Only delay if we are too fast too process this frame
-    if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
-        SDL_Delay(time_to_wait);
-    }
-
-    // Get a delta time factor converted to seconds to be used to update my objects
-    float delta_time = (SDL_GetTicks() - last_frame_time) / 1000.0f;
-
-    // Store the milliseconds of the current frame
-    last_frame_time = SDL_GetTicks();
-
-    // Check for ball collision with the walls
-    if (ball.x + ball.width > WINDOW_WIDTH || ball.x < 0)
-        ball.vel_x = -ball.vel_x;
-    if (ball.y < 0)
-        ball.vel_y = -ball.vel_y;
-
-    // Check for ball collision with the paddle
-    if (ball.y + ball.height >= paddle.y && ball.x + ball.width > paddle.x && ball.x < paddle.x + paddle.width)
-        ball.vel_y = -ball.vel_y;
-
-    // Check for game over
-    if (ball.y + ball.height > WINDOW_HEIGHT)
-        game_is_running = FALSE;
-
-    // update ball position
-    ball.x += ball.vel_x * delta_time;
-    ball.y += ball.vel_y * delta_time;
-
-    // update paddle position
-    paddle.x += paddle.vel_x * delta_time;
-    paddle.y += paddle.vel_y * delta_time;
+void update() {
+  // todo
 }
 
-void render(void) {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+void render() {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
 
-    // Draw a rectangle
-    SDL_Rect ball_rect = {
-        (int)ball.x,
-        (int)ball.y,
-        (int)ball.width,
-        (int)ball.height
-    };
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &ball_rect);
+  // draw a rectangle
+  SDL_Rect ball_rect = {
+    ball.x,
+    ball.y,
+    ball.width,
+    ball.heigth
+  };
 
-    // Render the left paddle
-    SDL_Rect paddle_rect = {
-        (int)paddle.x,
-        (int)paddle.y,
-        (int)paddle.width,
-        (int)paddle.height
-    };
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &paddle_rect);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderFillRect(renderer, &ball_rect);
 
-    SDL_RenderPresent(renderer);
+  SDL_RenderPresent(renderer);
 }
 
-void destroy_window(void) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
+void destroy_window() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+};
 
-int main(int argc, char **argv) {
-    game_is_running = initialize_window();
+int main() {
+  game_is_running = initialize_window();
+  printf("game is running\n");
 
-    setup();
+  setup();
 
-    while (game_is_running) {
-        process_input();
-        update();
-        render();
-    }
+  while(game_is_running) {
+    process_input();
+    update();
+    render();
+  }
 
-    destroy_window();
+  destroy_window();
 
-    return 0;
+  return 0;
 }
---
+++ java/coursera-algorithms-part-1/RandomWord.java
@@ -0,0 +1,7 @@
+class RandomWord {
+    public static void main() {
+        
+        System.out.println("Hello " + args[0] + " and " + args[1] + ".");
+        // System.out.println("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
