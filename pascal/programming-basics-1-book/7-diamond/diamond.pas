813e2e413eba48f2d9bc62dbecd5f5dfa2db7d4e
alexander.mospan <alexander.mospan@smartengines.ru>
2025-09-17 11:36:20 +0300

    update

--- pascal/programming-basics-1-book/7-diamond/diamond.pas
+++ pascal/programming-basics-1-book/7-diamond/diamond.pas
@@ -1,19 +1,42 @@
 // Программа, которая выводит ромб заданной высоты из символа *
-// Программа принимает только положительные целые нечётные числа.
+// Принимает только положительные целые нечётные числа.
 program diamond;
 
 var
-  height: integer;
+  size: integer;
+  center: integer;
+  i: integer;
+  j: integer;
 
 begin
-  write('Enter the diamond height (positive odd): ');
-  read(height);
+  write('Enter the diamond size (positive odd): ');
+  read(size);
 
-  if height < 1 then
-    writeln('Number should be positive.')
-  else if height mod 2 = 0 then
-    writeln('Number should be odd.')
+  if size < 1 then
+    writeln('Error: only positive numbers accepted.')
+  else if size mod 2 = 0 then
+    writeln('Error: only odd numbers accepted.')
   else begin
-    
+    center := size div 2 + 1;
+
+    for i:= 1 to center do begin
+      for j:= 1 to size do begin
+        if (j = center - i + 1) or (j = size - center + i) then
+          write('*')
+        else
+          write(' ');
+      end;
+      writeln('');
+    end;
+
+    for i:= 1 to center - 1 do begin
+      for j:= 1 to size do begin
+        if (j = i + 1) or (j = size - i) then
+          write('*')
+        else
+          write(' ');
+      end;
+      writeln('');
+    end;
   end;
 end.
